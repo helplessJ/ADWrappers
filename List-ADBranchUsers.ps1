0a9e6fd3887d5dfe3c3e8d50265606cf4e3b7583
helplessJ <helplessJ@users.noreply.github.com>
2016-08-01 15:42:16 -0700

    first commit

---
+++ List-ADBranchUsers.ps1
@@ -0,0 +1,38 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.127
+	 Created on:   	7/29/2016 7:04 PM
+	 Created by:   	hawrylj
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+Import-Module ActiveDirectory
+
+function List-ADBranchUsers
+{
+
+	[cmdletbinding()]
+	param (
+		
+		[Parameter(Mandatory = $true)]
+		[string]$Branch = $null,
+		
+		$Properties = @('title','LastLogonDate')
+	)
+	
+	BEGIN
+	{
+		$targetOU = Get-ADOrganizationalUnit -Filter { Name -eq 'Users' } | Where-Object { $_.distinguishedName -match $Branch }
+	}
+	PROCESS
+	{
+		
+		foreach ($userOU in $targetOU) {
+			Get-ADUser -Filter * -SearchBase $userOU -Properties $Properties | Where-Object {$_.enabled -eq $true}
+		}
+	}
+}
