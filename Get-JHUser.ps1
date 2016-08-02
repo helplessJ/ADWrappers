26bf453be34b0d878152baf7669189defb46d05d
helplessJ <helplessJ@users.noreply.github.com>
2016-08-02 07:54:44 -0700

    adding an ADUser wrapper that takes a distinguished name and queeries the correct forest based on it

---
+++ Get-JHUser.ps1
@@ -0,0 +1,30 @@
+﻿Function Pull-DomainNameFromDN {
+
+  param($dn)
+  $eleCount = ($dn -split ',').Count
+  ($dn -split ',')[$eleCount - 2]
+}
+
+Function Get-JhUser {
+  [cmdletbinding()]
+    param(
+            [parameter(mandatory = $true)]
+            [string]$DistinguishedUsername
+          )
+
+  BEGIN{}
+  PROCESS
+  {
+    $userDom = Pull-DomainNameFromDN -dn $DistinguishedUsername
+    IF($userDom -match 'ehm')
+    {
+      $domainName = 'ehm.com'
+    } elseif($userDom -match 'uamc')
+    {
+      $domainName = 'uamc.com'
+    }
+    Get-ADUser $DistinguishedUsername -Server $domainName
+  }
+  END{}
+
+}
